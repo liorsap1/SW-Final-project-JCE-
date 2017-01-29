1d5059cbafee02984afb9f57ab4912869ec5a9dc
liorsap1 <liorsap1@gmail.com>
2017-01-29 20:56:05 +0200

    Creating java file for handle the page

--- Code/WifiApp2/app/src/main/java/com/example/and/wifiapp/Indices.java
+++ Code/WifiApp2/app/src/main/java/com/example/and/wifiapp/Indices.java
@@ -1,20 +1,36 @@
 package com.example.and.wifiapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by and on 1/28/2017.
  */
 
+
 public class Indices extends AppCompatActivity {
-    
+
+    Button backToMenu;
+    final Context context = this;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.indices);
+        backToMenu = (Button)findViewById(R.id.back_menu);
+
+        backToMenu.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
     private int getWifiStrength() {

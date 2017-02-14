a31380a9fd5b05da196faf325bbdc31daa2f0865
liorsap1 <liorsap1@gmail.com>
2017-02-14 16:49:06 +0200

    getting the range of the signal

--- Code/WifiApp2/app/src/main/java/com/example/and/wifiapp/Indices.java
+++ Code/WifiApp2/app/src/main/java/com/example/and/wifiapp/Indices.java
@@ -8,6 +8,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by and on 1/28/2017.
@@ -17,6 +18,8 @@ import android.widget.Button;
 public class Indices extends AppCompatActivity {
 
     Button backToMenu;
+    Button refresh;
+    TextView signal;
     final Context context = this;
 
     @Override
@@ -24,6 +27,8 @@ public class Indices extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.indices);
         backToMenu = (Button)findViewById(R.id.back_menu);
+        signal = (TextView)findViewById(R.id.range);
+        refresh = (Button)findViewById(R.id.refresh);
 
         backToMenu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -32,18 +37,25 @@ public class Indices extends AppCompatActivity {
             }
         });
 
+        getWifiStrength();
+        refresh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                getWifiStrength();
+            }
+        });
+
     }
-    private int getWifiStrength() {
+    private void getWifiStrength() {
         try {
             WifiManager wifiManager = (WifiManager) getApplicationContext()
                     .getSystemService(Context.WIFI_SERVICE);
             int rssi = wifiManager.getConnectionInfo().getRssi();
-            int level = WifiManager.calculateSignalLevel(rssi, 10);
-            int percentage = (int) ((level/10.0)*100);
-            return percentage;
+            int level = WifiManager.calculateSignalLevel(rssi, 100);
+            int percentage = (int) ((level/100.0)*100);
+            signal.setText("The signal is: "+percentage+"%");
         }
         catch (Exception e) {
-            return -1;
+            signal.setText("The signal is: "+"problem occure");
         }
     }
 }

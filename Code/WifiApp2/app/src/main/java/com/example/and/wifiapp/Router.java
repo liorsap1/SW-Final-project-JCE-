30fe5791658c45fc5167d42064d3afd372540de9
liorsap1 <liorsap1@gmail.com>
2017-04-17 19:47:56 +0300

    Creating connection between the xml to java file

--- Code/WifiApp2/app/src/main/java/com/example/and/wifiapp/Router.java
+++ Code/WifiApp2/app/src/main/java/com/example/and/wifiapp/Router.java
@@ -7,6 +7,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.PrintStream;
+import java.net.ServerSocket;
 
 /**
  * Created by and on 12/30/2016.
@@ -36,11 +41,90 @@ public class Router extends AppCompatActivity {
     final Context context = this;
 
 
+
+
+
+
+    //SERVER
+
+    TextView info, infoip, msg;
+    String message = "";
+    ServerSocket serverSocket;
+
+
+    //CLIENT
+
+    TextView textResponse;
+    EditText Address, Port;
+    Button buttonConnect, buttonClear;
+    PrintStream printStream;
+
+
+
+
+
+
+
+
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.config_page);
         backMenu = (Button)findViewById(R.id.back_menu);
+
+
+
+
+
+
+        //-----------SERVER-----------
+        info = (TextView) findViewById(R.id.info);
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+
+
+        //-----------CLIENT-----------
+        Address = (EditText) findViewById(R.id.address);
+        Port = (EditText) findViewById(R.id.port);
+        buttonConnect = (Button) findViewById(R.id.connect);
+        buttonClear = (Button) findViewById(R.id.clear);
+        textResponse = (TextView) findViewById(R.id.response);
+
+
+        //-----------CLIENT WORKING-----------
+        buttonConnect.setOnClickListener(ConnectListener);
+
+        //-----------SERVER WORKING-----------
+        infoip.setText(getIpAddress());
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+
+        buttonClear.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                textResponse.setText("");
+                msg.setText("");
+            }});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         backMenu.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
